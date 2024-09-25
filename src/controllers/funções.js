d61ed9569c36427d391dc3ce06f9ba323922b358
sandroluisj <sandroluisjr06@gmail.com>
2024-09-25 14:28:44 -0300

    ADD

--- src/controllers/funções.js
+++ src/controllers/funções.js
@@ -1,52 +1,59 @@
-const express = require("../models/controletimes")
+const Time = require("../models/controletimes");
 
 async function criarTime(req, res) {
     const { nome, valor, artilheiros } = req.body;
     try {
-        const novoTimes = new Time({ nome, valor, artilheiros });
-        const novoTime = await novoTimes.save();
-    res.status(201).json({ mensagem: "Time criado com sucesso", time: novoTime });
-  } catch (erro) {
-    res.status(500).json({ mensagem: "Erro ao criar time", erro: erro.message });
-  }}
-  async function listarTimes(req,res ){
-  try {
-    const times = await Time.find();
-    res.status(200).json(times);
-  } catch (erro) {
-    res.status(500).json({ mensagem: "Erro ao obter times", erro: erro.message });
-  }
-  }
-  async function atualizarTime (req, res){
-    const { id } = req.params;
-    const { nome, valor, artilheiro } = req.body;
-    try{
-        const timeAtualizado = await Time.findByIdAndUpdate(
-        id,
-        {nome, valor, artilheiro},
-        { new: true, runValidators: true })
-    if (timeAtualizado) {
-      res.status(200).json({
-        mensagem: "Time atualizado com sucesso!",
-        Time: timeAtualizado,
-      });
-    } else {
-      res.status(404).json({ mensagem: "O time não foi encontrado..." });
+        const novoTime = new Time({ nome, valor, artilheiros });
+        const timeSalvo = await novoTime.save();
+        res.status(201).json({ mensagem: "Time criado com sucesso", time: timeSalvo });
+    } catch (erro) {
+        res.status(500).json({ mensagem: "Erro ao criar time", erro: erro.message });
     }
-  } catch (erro) {
-    res.status(500).json({ mensagem: "Erro na atualização do time", erro: erro.message });
-  }
 }
-async function deletarTime(req, res){
-const { id } = req.params;
-try {
-    const timeDeletado = await Time.findByIdAndUpdate(id)
-    if (timeDeletado) {
-      res.status(200).json({ mensagem: "Time deletado com sucesso", Time: timeDeletado });
-    } else {
-      res.status(404).json({ mensagem: "O time não foi encontrado..." });
+
+async function listarTimes(req, res) {
+    try {
+        const times = await Time.find();
+        res.status(200).json(times);
+    } catch (erro) {
+        res.status(500).json({ mensagem: "Erro ao obter times", erro: erro.message });
     }
-  } catch (erro) {
-    res.status(500).json({ mensagem: "Erro ao deletar o time", erro: erro.message });
-  }}
-  module.exports = {criarTime, listarTimes, atualizarTime, deletarTime}
+}
+
+async function atualizarTime(req, res) {
+    const { id } = req.params;
+    const { nome, valor, artilheiros } = req.body; // Alterar para artilheiros se necessário
+    try {
+        const timeAtualizado = await Time.findByIdAndUpdate(
+            id,
+            { nome, valor, artilheiros }, // Alterar para artilheiros se necessário
+            { new: true, runValidators: true }
+        );
+        if (timeAtualizado) {
+            res.status(200).json({
+                mensagem: "Time atualizado com sucesso!",
+                time: timeAtualizado,
+            });
+        } else {
+            res.status(404).json({ mensagem: "O time não foi encontrado..." });
+        }
+    } catch (erro) {
+        res.status(500).json({ mensagem: "Erro na atualização do time", erro: erro.message });
+    }
+}
+
+async function deletarTime(req, res) {
+    const { id } = req.params;
+    try {
+        const timeDeletado = await Time.findByIdAndDelete(id);
+        if (timeDeletado) {
+            res.status(200).json({ mensagem: "Time deletado com sucesso", time: timeDeletado });
+        } else {
+            res.status(404).json({ mensagem: "O time não foi encontrado..." });
+        }
+    } catch (erro) {
+        res.status(500).json({ mensagem: "Erro ao deletar o time", erro: erro.message });
+    }
+}
+
+module.exports = { criarTime, listarTimes, atualizarTime, deletarTime };
