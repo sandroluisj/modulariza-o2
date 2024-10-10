d6ad26e1f25c2d2c5f67985c2af56c094bc22941
sandro luis de almeida junior <sandroluisjr06@gmail.com>
2024-10-10 10:51:44 -0300

    Update times.js

--- src/routes/times.js
+++ src/routes/times.js
@@ -1,8 +1,16 @@
-const express = require("express");
-const router = express.Router();
-const timescontroll = require("../controllers/funções");
-router.post("/", timescontroll.criarTime)
-router.get("/", timescontroll.listarTimes)
-router.put("/:id", timescontroll.atualizarTime)
-router.delete("/:id", timescontroll.deletarTime)
-module.exports = router
+import { Router } from 'express';
+import {
+    store,
+    index,
+    update,
+    destroy
+} from '../controllers/enterroController.js'; 
+
+const router = Router();
+
+router.post('/', store);
+router.get('/', index);
+router.put('/:id', update);
+router.delete('/:id', destroy);
+
+export default router;
